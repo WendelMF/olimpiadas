633051b8ccaf3787e447c6a84299c24bc9c45a5f
WendelMF <wendelsilvadev@gmail.com>
2024-09-08 16:54:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa")
+console.log (section)
+
+section.innerHTML =`
+  <div class="item-resultado">
+    <h2>
+       <a href="#" target="_blank">Rebeca Andrade</a> 
+</h2>
+<p class="descricao-meta">Rebeca Rodrigues de Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.</p>
+<a href="https://pt.wikipedia.org/wiki/Rebeca_Andrade"target="_blank">Mais Informações</a>
+</Div>
+`
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados= [
+    {
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Rodrigues de Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional.",
+    link : "https://pt.wikipedia.org/wiki/Rebeca Andrade"
+},
+{
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, também conhecida carinhosamente como Fadinha do Skate, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Nascida em Imperatriz, no Maranhão, em 2008, Rayssa começou a andar de skate ainda criança e rapidamente se destacou no cenário internacional.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+    {
+    titulo : "Beatriz Souza",
+    descricao : "Beatriz Souza é uma judoca brasileira que vem conquistando o cenário internacional e se consolidando como uma das maiores promessas do esporte. Com sua técnica impecável e determinação, Beatriz tem se destacado nas competições e trazendo orgulho para o Brasil.",
+    link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+}
+]
